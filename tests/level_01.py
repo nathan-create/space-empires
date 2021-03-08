735ba0d95ecc075d36dfc94cb5cb48b691ed57b7
NathanReynoso <sourlemonz111@gmail.com>
2021-03-08 03:25:59 +0000

    made level 0.1

---
+++ tests/level_01.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('level_01')
+from game_class import *
+from player_class import *
+
+# test A
+players = [CustomPlayer(), CustomPlayer()]
+game = Game(players)
+
+game.move_phase()
+game.combat_phase()
+game.move_phase()
+game.combat_phase()
+game.move_phase()
+game.combat_phase()
+print(game.state['players'])
+
+# test B
+num_wins = {1: 0, 2: 0}
+for _ in range(200):
+    players = [CustomPlayer(), CustomPlayer()]
+    game = Game(players)
+    game.run_to_completion()
+    winner = game.state['winner']
+    num_wins[winner] += 1
+print(num_wins)
+if abs(num_wins[1] - num_wins[2]) > 40:
+    print("there is a problem")
